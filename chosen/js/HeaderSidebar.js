a590e7052b1d833cfa88973d79e94b69a163197e
Ibrahim Sicid Jama <92254565+softwarepioneers@users.noreply.github.com>
2024-11-11 16:50:45 +0300

    ENGLISH

---
+++ chosen/js/HeaderSidebar.js
@@ -0,0 +1,13 @@
+class SpecialHeader extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML=''
+    }
+}
+
+class SpecialSidebar extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML=''
+    }
+}
+
+customElements.define(''.)
